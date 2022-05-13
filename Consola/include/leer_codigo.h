eaa8042325e669f15d9c6f8ba5a585fdf2c5c979
juliadp <jdalessiopoisson@frba.utn.edu.ar>
2022-05-13 00:54:44 -0300

    Enviamos NO_OP la cantidad de veces del parametro

--- Consola/include/leer_codigo.h
+++ Consola/include/leer_codigo.h
@@ -7,17 +7,8 @@
 
 #define LARGO_LINEA_MAX 20
 
-static char codigos_instrucciones[6][6] = {
-    "NO_OP",
-    "I/O",
-    "READ",
-    "WRITE",
-    "COPY",
-    "EXIT"
-};
-
-t_instruccion* string_a_instruccion(char* linea);
 char* leer_instruccion(FILE *f);
 t_paquete* leer_archivo(char *path);
+void agregar_instruccion_a_paquete(char* linea, t_paquete* paquete);
 
 #endif
